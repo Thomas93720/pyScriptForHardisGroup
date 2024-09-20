d83c6e0ee9b5cc1ce2503e9dce7b90dfb52b7fc3
Thomas Przt <thomas.prezot@hesias.fr>
2024-09-20 14:26:40 +0200

    ajout configuration du projet

---
+++ setup.py
@@ -0,0 +1,10 @@
+'''
+Installation du projet en une simple commande grace à un setup
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name="pyScriptForHardisGroup",
+    version="1.0",
+    packages=find_packages(),
+)
