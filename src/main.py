4d171325f3345c75c84ea86a2fd52759eb3d2364
Thomas Przt <thomas.prezot@hesias.fr>
2024-09-21 23:48:56 +0200

    modification du main pour lancer le programme en ayant tout assemblé

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from utils import parse_arguments
+from utils import parse_arguments,format_url,http_get
 
 if __name__ == "__main__":
     '''
     Appel des fonctions uniquement lorsque le script sera exécuté directement.
     '''
     arguments = parse_arguments()
-    print(arguments)
+    Protocol = arguments.protocol
+    Hostname = arguments.hostname
+    Uri = arguments.uri
+    Threshold = arguments.threshold
+    FormattedUrl = format_url(
+        protocol=Protocol,
+        hostname=Hostname,
+        uri=Uri,
+    )
+    print(
+        http_get(
+            url=FormattedUrl,
+            threshold=Threshold,
+        )
+    )
