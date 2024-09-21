e542393d737f1e20173af1437abe083745f0d250
Thomas Przt <thomas.prezot@hesias.fr>
2024-09-21 14:06:34 +0200

    enlever les test d'urls invalide et valider uniquement le bon formatage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,24 +16,5 @@ class TestFormatUrl(unittest.TestCase):
         self.assertEqual(format_url("https", "www.impots.gouv.fr", "/exemple/de/redirection/"), "https://www.impots.gouv.fr/exemple/de/redirection/")
         self.assertEqual(format_url("https", "8.8.8.8", ""), "https://8.8.8.8")
 
-    def test_urls_invalid(self):
-        '''
-        test des cas avec des urls invalides
-        '''
-        InvalidCases = [
-            ("ftp", "google.com", "/fr"),
-            ("protocole", "google.com", "/fr"),
-            (123,'google.com','/fr'),
-            (None,'google.com','/fr'),
-            ('https',123,'/fr'),
-            ('https',None,'/fr'),
-            ('https','google.com',123),
-            ('https','google.com',None),
-        ]
-
-        for protocol, hostname, uri in InvalidCases:
-            with self.assertRaises(ValueError):
-                format_url(protocol, hostname, uri)
-
 if __name__ == "__main__":
     unittest.main()
